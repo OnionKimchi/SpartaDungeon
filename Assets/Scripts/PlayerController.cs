40ca6494b31ce46897fcb837aef4516fa49b6270
OnionKimchi <jungjung21b@gmail.com>
2025-05-19 12:39:22 +0900

    움직임 추가
    플레이어 컨트롤러 스크립트

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
-    public
+    public float moveSpeed;
+    [SerializeField] private Vector2 curMovementInput;
+
+    [Header("Look")]
+    public Transform cameraContainer;
+    public float minLookX;
+    public float maxLookX;
+    [SerializeField] private float camCurXRotation;
+    public float camSensitivity;
+    [SerializeField] private Vector2 mouseDelta;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = rb.velocity.y;
+
+        rb.velocity = dir;
+    }
+
+    void CameraLook()
+    {
+        camCurXRotation += mouseDelta.y * camSensitivity;
+        camCurXRotation = Mathf.Clamp(camCurXRotation, minLookX, maxLookX);
+        cameraContainer.localRotation = Quaternion.Euler(-camCurXRotation, 0, 0);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
     }
 }
