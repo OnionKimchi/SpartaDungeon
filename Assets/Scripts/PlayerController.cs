e88637e247efeb56e740d3b4f6d1c4f5efb20cf0
OnionKimchi <jungjung21b@gmail.com>
2025-05-19 17:26:27 +0900

    시선과 더블점프 구현
    플레이어 컨트롤러 스크립트 수정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,15 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
     [SerializeField] private Vector2 curMovementInput;
+    [SerializeField] private float jumpForce = 80f;
+    public LayerMask groundLayerMask;
+    [SerializeField] private int maxJumpCount = 1;
+    private int _jumpStack;
+    public int jumpStack
+    {
+        get => _jumpStack;
+        set => _jumpStack = Mathf.Clamp(value, 0, maxJumpCount);
+    }
 
     [Header("Look")]
     public Transform cameraContainer;
@@ -28,11 +37,19 @@ public class PlayerController : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         Move();
+        // 땅에 닿았으면 점프 스택 복구
+        if (IsGrounded())
+        {
+            jumpStack = maxJumpCount;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        CameraLook();
     }
 
     void Move()
@@ -49,6 +66,8 @@ public class PlayerController : MonoBehaviour
         camCurXRotation += mouseDelta.y * camSensitivity;
         camCurXRotation = Mathf.Clamp(camCurXRotation, minLookX, maxLookX);
         cameraContainer.localRotation = Quaternion.Euler(-camCurXRotation, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * camSensitivity, 0);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -70,5 +89,46 @@ public class PlayerController : MonoBehaviour
     public void OnLook(InputAction.CallbackContext context)
     {
         mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            if (IsGrounded())
+            {
+                // 땅에 닿았을 때 점프
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+            else if (jumpStack > 0)
+            {
+                // 공중 점프
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                jumpStack--;
+            }
+        }
+    }
+
+    bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up*0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up*0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (transform.up*0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + (transform.up*0.01f), Vector3.down)
+        };
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
